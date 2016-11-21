6db4c5748ee30f327700f14663980e644f9ad06c
Сорокин Михаил Атрёмович <sorokin.michael.96@yandex.ru>
2016-11-21 13:24:56 +0300

    Сделаны поиск группы и пользователя;добавлены права на исполнения некоторым файлам

--- find_group.sh
+++ find_group.sh
@@ -1,40 +1,85 @@
 #!/bin/bash
+function found_user_help () {
+echo "Для выхода в меню нажмите --back или -b
+Для вызова справки --help или -h
+"
+}
 who=$(whoami)
 case $who in 
 	"root")# делай всё если пользователь root
-;;
-	"mic")
+TOTAL_LOOP=1
+while [[ $TOTAL_LOOP -eq 1 ]]; do
+
 echo "Введите имя группы или часть имени"
 # проверка на пустой ввод
 while read input_name 
 do
 	input_len=${#input_name}
 	if [ $input_len -eq 0 ] ; then
-		echo "Вы ввели пустую строку!\nВведите имя группы или часть имени"
+		echo "Вы ввели пустую строку!
+Введите имя группы или часть имени"
 	else break;
 	fi
 done
 
 case $input_name in
-	"--help")
+	"--help"|"-h"*)
+		found_user_help
+		continue
 		;;
-	"--back");;
+	"--back"|"-b")
+		exit 0;;
 	*);;
 esac
 
 #поиск групп с именем, начинающимся на input_name
-groupnames="$(grep -i ^$input_name  /etc/group | cut -d: -f1)"
+groupnames="$(grep -i ^$input_name  /etc/group )"
 len=${#groupnames}
 # если что-то найдено
 if [ $len -ne 0  ] ; then
-	for group in $groupnames
-	do
-		echo "Имя группы=$group"
-		
-	done
+	while IFS=: read name password GID oter; do
+		echo "Имя группы $name
+ GID= $GID"
+		participants="$(lid -g $name)"
+		if [ ${#participants} -ne 0 ] ; then
+			echo " Пользователи группы:"
+			for member in $participants
+			do
+				echo " $member"
+			done
+		else echo "Пользователей не найдено."
+		fi
+	done <<< "$groupnames"
 else
 echo "Групп не найдено."
 fi
+
+MICRO_LOOP=1
+
+while [[ $MICRO_LOOP -eq 1 ]]; do
+read -p  "Найти группу ещё?(y/n) " response
+
+case $response in
+	[Nn]*)
+		echo "Выход в меню"
+		exit 0;;
+	[Yy]*)
+		TOTAL_LOOP=1
+		break;;
+	"--help"|"-h")
+		found_user_help
+		continue
+		;;
+	"--back"|"-b")
+		exit 0;;
+	*)
+		echo "Введите y/n"
+esac
+
+done
+
+
+done
 ;;
 	*) 
 echo "Выполните вход в root"
--- find_user.sh
+++ find_user.sh
@@ -1,24 +1,35 @@
 #!/bin/bash
+function found_user_help () {
+echo "Для выхода в меню нажмите --back или -b
+Для вызова справки --help или -h
+"
+}
+
 who=$(whoami)
 case $who in 
 	"root")# делай всё если пользователь root
-;;
-	"mic")
+TOTAL_LOOP=1
+while [[ $TOTAL_LOOP -eq 1 ]]; do
+
 echo "Введите имя пользователя или часть имени"
 # проверка на пустой ввод
 while read input_name 
 do
 	input_len=${#input_name}
 	if [ $input_len -eq 0 ] ; then
-		echo "Вы ввели пустую строку!\nВведите имя пользователя или часть имени"
+		echo "Вы ввели пустую строку!
+Введите имя пользователя или часть имени"
 	else break;
 	fi
 done
 
 case $input_name in
-	"--help")
+	"--help"|"-h")
+		found_user_help
+		continue
 		;;
-	"--back");;
+	"--back"|"-b")
+		exit 0;;
 	*);;
 esac
 
@@ -27,16 +38,43 @@ usernames="$(grep -i ^$input_name  /etc/passwd | cut -d: -f1)"
 len=${#usernames}
 # если что-то найдено
 if [ $len -ne 0  ] ; then
+	
 	for user in $usernames
 	do
-		echo "Имя пользователя=$user"
-		id $user # далее будем использовать lslogins (работает только на Авроре)
+		lslogins $user
 	done
+
 else
 echo "Пользователей не найдено."
 fi
+MICRO_LOOP=1
+
+while [[ $MICRO_LOOP -eq 1 ]]; do
+read -p  "Найти пользователя ещё?(y/n) " response
+
+case $response in
+	[Nn]*)
+		echo "Выход в меню"
+		exit 0;;
+	[Yy]*)
+		TOTAL_LOOP=1
+		break;;
+	"--help"|"-h")
+		found_user_help
+		continue
+		;;
+	"--back"|"-b")
+		exit 0;;
+	*)
+		echo "Введите y/n"
+esac
+
+done
+
+done
 ;;
 	*) 
 echo "Выполните вход в root"
 ;;
 esac
+
