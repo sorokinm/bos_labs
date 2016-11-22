0f69b4378f6e35764315f020f3ab9abb62811203
simpleman1997 <simpleman1997@localhost.localdomain>
2016-11-22 11:36:37 +0300

    Поправил баг по вводу

--- create_group.sh
+++ create_group.sh
@@ -5,8 +5,8 @@ TOTAL_LOOP=1
 
 while (( TOTAL_LOOP )); do
 
-echo "Cуществующие группы:
----------------------"
+echo "Cуществующие группы:"
+echo "--------------------"
 
 cut -d: -f1 /etc/group | sort -d  #выводит группы, которые уже есть
 echo "--------------------"
@@ -17,12 +17,44 @@ while (( LOOP )); do
 echo "Введите имя новой группы: (Введите q чтобы выйти)" #ввод названия группы
 read GROUP
 
+
+ISVALID=1
+if [ $GROUP == '-' ] || [ $GROUP == '+' ];
+	then
+		ISVALID=0
+fi
+
+SYMB=${GROUP:0:1}
+if [ $SYMB == '-' ] || [ $SYMB == '+' ];
+		then
+		ISVALID=0
+fi
+#-----
+#for I in $(seq 0 $((${#GROUP}-1))); 
+#do 
+#SYMB=${GROUP:$I:1}
+#if [ $SYMB == '-' ] || [ $SYMB == '+' ];
+#then 
+#	echo "true"
+#else
+#	echo "false"
+#fi
+#echo $SYMB;
+#done 
+ 
+
+
 case $GROUP in
 	"q") echo "Выход из программы"
 		 exit 0
 	 	 break;;
 
-	*) sudo groupadd $GROUP  #создает группу или выводит ошибку
+	*) if [ $ISVALID == '1' ]; 
+			then 
+				sudo groupadd $GROUP 
+			else
+				echo "Недопустимое имя"
+	   fi  #создает группу или выводит ошибку
 	   break;; 
 esac
 
