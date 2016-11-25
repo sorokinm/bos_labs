eb312efb829bfadb9ea6bda5cfe4ccfd14b0d188
ssbbkn <ssbbkn@localhost.localdomain>
2016-11-25 17:44:10 +0300

    Исправил вывод пользователей

--- delete_user.sh
+++ delete_user.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 who=$(whoami)
 case $who in
-	"root")	
-cut -d: -f1 /etc/passwd | cat -n | awk '$1=$1' > result.txt #вывести список пользователей, пронумеровав его и убрав повторяющиеся пробелы
+		"root")
+while ((1));do			
+cut -d: -f1,3 /etc/passwd | awk '$1=$1' | tr -s ' ' ':' | awk -F : '(($2>=1000)&&($1!="nfsnobody")){print $1}' | cat -n | awk '$1=$1' > result.txt #вывести список пользователей, пронумеровав его и убрав повторяющиеся пробелы
 cat result.txt
 echo "Введите имя пользователя или порядковый номер"
 read choose
+repeats=0
 grep -w $choose result.txt | cut -d' ' -f2 > findResult.txt #выбор из списка пользователей имени введенного пользователя
 findResultLen=$(stat -c%s findResult.txt) #определение количества символов в файле с именем пользователя (если длина файла 0, то вывести "Пользователь не найден")
 case $findResultLen in
@@ -13,7 +15,24 @@ case $findResultLen in
 	  echo "Скрипт закончил работу"
 		rm findResult.txt
 		rm result.txt
-		exit 0
+	        while ((1));do
+        echo "Повторить?(y/n)"
+        read repeat
+        case $repeat in
+                "y")$repeats=1
+                        break;;
+                "n")echo "Выполняется выход..."
+                        $repeats=0
+                        break;;
+                *)echo "Неверный ввод";;
+        esac
+		done
+	if (("$repeats"=="0"));then
+	break
+	elif (("$repeats"=="1"));then
+	continue
+	fi
+	
 		;;
 	*)echo "Найден пользователь: " 
 		cat findResult.txt
@@ -32,13 +51,45 @@ case $delChoose in
 	"n")echo "Скрипт закончил работу"
 		rm result.txt
 		rm findResult.txt
-		exit 0
+		while ((1));do
+        echo "Повторить?(y/n)"
+        read repeat
+        case $repeat in
+                "y")repeats=1
+                        break;;
+                "n")echo "Выполняется выход..."
+                        repeats=0
+                        break;;
+                *)echo "Неверный ввод";;
+        esac
+                done
+        if (("$repeats"=="0"));then
+        break
+        elif (("$repeats"=="1"));then
+        continue
+        fi
 		;;
 	*)echo "Неверный ввод"
 		echo "Скрипт закончил работу"
 		rm result.txt
 		rm findResult.txt
-		exit 0
+		while ((1));do
+        echo "Повторить?(y/n)"
+        read repeat
+        case $repeat in
+                "y")repeats=1
+                        break;;
+                "n")echo "Выполняется выход..."
+                        repeats=0
+                        break;;
+                *)echo "Неверный ввод";;
+        esac
+                done
+        if (("$repeats"=="0"));then
+        break
+        elif (("$repeats"=="1"));then
+        continue
+        fi
 		;;	
 esac	
 case $catChoose in
@@ -46,21 +97,86 @@ case $catChoose in
 		echo "Скрипт закончил работу"
 		rm result.txt
 		rm findResult.txt
-		exit 0
+		while ((1));do
+        echo "Повторить?(y/n)"
+        read repeat
+        case $repeat in
+                "y")repeats=1
+                        break;;
+                "n")echo "Выполняется выход..."
+                        repeats=0
+                        break;;
+                *)echo "Неверный ввод";;
+        esac
+                done
+        if (("$repeats"=="0"));then
+        break
+        elif (("$repeats"=="1"));then
+        continue
+        fi
 		;;
 	"n")userdel $foundUser
 		echo "Скрипт закончил работу"
 		rm result.txt
 		rm findResult.txt
-		exit 0
+		while ((1));do
+        echo "Повторить?(y/n)"
+        read repeat
+        case $repeat in
+                "y")repeats=1
+                        break;;
+                "n")echo "Выполняется выход..."
+                        repeats=0
+                        break;;
+                *)echo "Неверный ввод";;
+        esac
+                done
+        if (("$repeats"=="0"));then
+        break
+        elif (("$repeats"=="1"));then
+        continue
+        fi
 		;;
 	*)echo "Неверный ввод"
 		echo "Скрипт закончил работу"
 		rm result.txt
 		rm findResult.txt
-		exit 0
+		while ((1));do
+        echo "Повторить?(y/n)"
+        read repeat
+        case $repeat in
+                "y")repeats=1
+                        break;;
+                "n")echo "Выполняется выход..."
+                        repeats=0
+                        break;;
+                *)echo "Неверный ввод";;
+        esac
+                done
+        if (("$repeats"=="0"));then
+        break
+        elif (("$repeats"=="1"));then
+        continue
+        fi
 		;;
 esac
+while ((1));do
+	echo "Повторить?(y/n)"
+	read repeat
+	case $repeat in
+		"y")repeats=1
+			break;;
+		"n")echo "Выполняется выход..."
+			repeats=0
+			break;;
+		*)echo "Неверный ввод";;
+	esac	
+done
+if (("$repeats"=="0"));then
+break	
+fi	
+done
 ;;
-*)echo "Выполните вход в root";;
+*) echo "Выполните вход в root";;
 esac
+
