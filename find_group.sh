e549f433293a99781e8811f2c0b6c67747783598
Сорокин Михаил Атрёмович <sorokin.michael.96@yandex.ru>
2016-11-26 00:18:11 +0300

    Добавил отступы в скрипте

--- find_group.sh
+++ find_group.sh
@@ -1,92 +1,91 @@
 #!/bin/bash
 function found_user_help () {
-echo "Для выхода в меню нажмите --back или -b
+echo "
+
+Справка:
+Для выхода в меню нажмите --back или -b
 Для вызова справки --help или -h
+
 "
 }
 
 echo "---------------------------------------"
-who=$(whoami)
-case $who in 
-	"root")# делай всё если пользователь root
+
+if [[ $(whoami) != root ]]; then
+	echo "Выполните вход в root"
+	echo "---------------------------------------"
+	exit 1
+fi
+
 TOTAL_LOOP=1
 while [[ $TOTAL_LOOP -eq 1 ]]; do
 
-echo "Введите имя группы или часть имени"
-# проверка на пустой ввод
-while read input_name 
-do
-	input_len=${#input_name}
-	if [ $input_len -eq 0 ] ; then
-		echo "Вы ввели пустую строку!
-Введите имя группы или часть имени"
-	else break;
-	fi
-done
-
-case $input_name in
-	"--help"|"-h"*)
-		found_user_help
-		continue
-		;;
-	"--back"|"-b")
-		echo "---------------------------------------"
-		exit 0;;
-	*);;
-esac
-
-#поиск групп с именем, начинающимся на input_name
-groupnames="$(grep -i ^$input_name  /etc/group )"
-len=${#groupnames}
-# если что-то найдено
-if [ $len -ne 0  ] ; then
-	while IFS=: read name password GID oter; do
-		echo "Имя группы $name
- GID= $GID"
-		participants="$(lid -g $name)"
-		if [ ${#participants} -ne 0 ] ; then
-			echo " Пользователи группы:"
-			for member in $participants
-			do
-				echo " $member"
-			done
-		else echo "Пользователей не найдено."
+	echo "Введите имя группы или часть имени"
+	# проверка на пустой ввод
+	while read input_name 
+	do
+		input_len=${#input_name}
+		if [ $input_len -eq 0 ] ; then
+			printf "Вы ввели пустую строку!\nВведите имя группы или часть имени\n"
+		else break;
 		fi
-	done <<< "$groupnames"
-else
-echo "Групп не найдено."
-fi
+	done
 
-MICRO_LOOP=1
-
-while [[ $MICRO_LOOP -eq 1 ]]; do
-read -p  "Найти группу ещё?(y/n) " response
-
-case $response in
-	[Nn]*)
-		echo "Выход в меню"
-		echo "---------------------------------------"
-		exit 0;;
-	[Yy]*)
-		TOTAL_LOOP=1
-		break;;
-	"--help"|"-h")
-		found_user_help
-		continue
-		;;
+	case $input_name in
+	"--help"|"-h"*)
+			found_user_help
+			continue;;
 	"--back"|"-b")
-		exit 0;;
-	*)
-		echo "Введите y/n"
-esac
+			echo "---------------------------------------"
+			exit 0;;
+	*);;
+	esac
 
-done
+	#поиск групп с именем, начинающимся на input_name
+	groupnames="$(grep -i ^$input_name  /etc/group )"
+	len=${#groupnames}
+	# если что-то найдено
+	if [ $len -ne 0  ] ; then
+		while IFS=: read name password GID oter; do
+			printf "Имя группы $name\n GID= $GID \n"
+			participants="$(lid -g $name)"
+			if [ ${#participants} -ne 0 ] ; then
+				echo " Пользователи группы:"
+				for member in $participants
+				do
+					echo " $member"
+				done
+				printf "\n"
+			else printf "Пользователей не найдено.\n\n"
+			fi
+		done <<< "$groupnames"
+	else echo "Групп не найдено."
+	fi
 
+	MICRO_LOOP=1
 
-done
-;;
-	*) 
-echo "Выполните вход в root"
-;;
-esac
+	while [[ $MICRO_LOOP -eq 1 ]]; do
+		read -p  "Повторить?(y/n) " response
+
+		case $response in
+			[Nn]*)
+				echo "Выход в меню"
+				echo "---------------------------------------"
+				exit 0;;
+			[Yy]*)
+				TOTAL_LOOP=1
+				break;;
+		"--help"|"-h")
+				found_user_help
+				continue;;
+		"--back"|"-b")
+				exit 0;;
+				*)
+				echo "Введите y/n"
+		esac
+
+	done
+
+done 
+
 echo "---------------------------------------"
--- find_user.sh
+++ find_user.sh
@@ -1,83 +1,83 @@
 #!/bin/bash
 function found_user_help () {
-echo "Для выхода в меню нажмите --back или -b
+echo "
+
+Справка:
+Для выхода в меню нажмите --back или -b
 Для вызова справки --help или -h
+
 "
 }
 
 echo "---------------------------------------"
-who=$(whoami)
-case $who in 
-	"root")# делай всё если пользователь root
+if [[ $(whoami) != root ]]; then
+	echo "Выполните вход в root"
+	echo "---------------------------------------"
+	exit 1
+fi
+
 TOTAL_LOOP=1
 while [[ $TOTAL_LOOP -eq 1 ]]; do
 
-echo "Введите имя пользователя или часть имени"
-# проверка на пустой ввод
-while read input_name 
-do
-	input_len=${#input_name}
-	if [ $input_len -eq 0 ] ; then
-		echo "Вы ввели пустую строку!
-Введите имя пользователя или часть имени"
-	else break;
-	fi
-done
-
-case $input_name in
-	"--help"|"-h")
-		found_user_help
-		continue
-		;;
-	"--back"|"-b")
-		echo "---------------------------------------"
-		exit 0;;
-	*);;
-esac
-
-#поиск пользователей с именем, начинающимся на input_name
-usernames="$(grep -i ^$input_name  /etc/passwd | cut -d: -f1)"
-len=${#usernames}
-# если что-то найдено
-if [ $len -ne 0  ] ; then
-	
-	for user in $usernames
+	echo "Введите имя пользователя или часть имени"
+	# проверка на пустой ввод
+	while read input_name 
 	do
-		lslogins $user
+		input_len=${#input_name}
+		if [ $input_len -eq 0 ] ; then
+			printf "Вы ввели пустую строку!\nВведите имя пользователя или часть имени\n"
+		else break;
+		fi
 	done
 
-else
-echo "Пользователей не найдено."
-fi
-MICRO_LOOP=1
+	case $input_name in
+		"--help"|"-h")
+			found_user_help
+			continue
+			;;
+		"--back"|"-b")
+			echo "---------------------------------------"
+			exit 0;;
+		*);;
+	esac
 
-while [[ $MICRO_LOOP -eq 1 ]]; do
-read -p  "Найти пользователя ещё?(y/n) " response
+	#поиск пользователей с именем, начинающимся на input_name
+	usernames="$(grep -i ^$input_name  /etc/passwd | cut -d: -f1)"
+	len=${#usernames}
+	# если что-то найдено
+	if [ $len -ne 0  ] ; then
+	
+		for user in $usernames
+		do
+			lslogins $user
+		done
+	else
+		echo "Пользователей не найдено."
+	fi
+	
+	MICRO_LOOP=1
 
-case $response in
-	[Nn]*)
-		echo "Выход в меню"
-		echo "---------------------------------------"
-		exit 0;;
-	[Yy]*)
-		TOTAL_LOOP=1
-		break;;
+	while [[ $MICRO_LOOP -eq 1 ]]; do
+		read -p  "Повторить?(y/n) " response
+
+		case $response in
+			[Nn]*)
+				echo "Выход в меню"
+				echo "---------------------------------------"
+				exit 0;;
+			[Yy]*)
+				TOTAL_LOOP=1
+				break;;
 	"--help"|"-h")
-		found_user_help
-		continue
-		;;
+				found_user_help
+				continue;;
 	"--back"|"-b")
-		exit 0;;
-	*)
-		echo "Введите y/n"
-esac
+				exit 0;;
+				*)
+				echo "Введите y/n"
+			esac
 
-done
-
-done
-;;
-	*) 
-echo "Выполните вход в root"
-;;
-esac
+	done
+done 
 echo "---------------------------------------"
+
