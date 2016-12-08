cca38d6340df3d8c68a2a026a680d1b3d0badfa3
simpleman383 <simpleman383@gmail.com>
2016-12-08 22:37:24 +0300

    Переношу на маcтер

--- delete_group.sh
+++ delete_group.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 #delete group
 
+ME=$(whoami)
+
+case $ME in
+	"root")
+
 TOTAL_LOOP=1
 
 while (( TOTAL_LOOP )); do
@@ -17,12 +22,51 @@ while (( LOOP )); do
 echo "Введите имя группы, которую хотите удалить: (Введите q чтобы выйти)" #ввод названия группы
 read GROUP
 
+
+
+
+#input validation	
+ISVALID=1					
+
+if [ -z $GROUP ];
+	then
+		ISVALID=0
+fi
+
+for I in $(seq 0 $((${#GROUP}-1))); 			
+do 
+SYMB=${GROUP:$I:1}
+
+if [ $I -eq 0 ] 
+	then
+		if [ $SYMB == "-" ] || [ $SYMB == "+" ] || [ -z $SYMB  ];
+			then 
+				ISVALID=0
+				break
+		fi
+	else
+		if [ -z $SYMB  ];
+			then 
+				ISVALID=0
+				break
+		fi
+
+fi
+done 
+#input validation	 
+
+
 case $GROUP in
 	"q") echo "Выход из программы"
 		 exit 0
 	 	 break;;
 
-	*) sudo groupdel $GROUP  #удаляет группу или выводит ошибку
+	*) if [ $ISVALID -eq 1 ]; 
+	   		then
+				sudo groupdel $GROUP  #удаляет группу или выводит ошибку
+	   		else
+				echo "Недопустимое имя"
+	   fi
 	   break;; 
 esac
 
@@ -50,3 +94,8 @@ done
 
 
 done
+break;;
+
+*) echo "Вы не являетесь root-пользователем"
+
+esac
