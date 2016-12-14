89a59df7678b2aa185268957bf89bddf9ce0e2ed
root <root@localhost.localdomain>
2016-12-14 14:17:52 +0300

    Добавил полосочки-разделители, переделал использование временных файлов, исправил баг с блокировкой.

--- block_user.sh
+++ block_user.sh
@@ -1,20 +1,25 @@
 #!/bin/bash
+echo "---------------------------------------"
 if [[ $(whoami) != root ]]; then
 	echo "Выполните вход в root"
+echo "---------------------------------------"
 	exit 1
 fi
 while ((1));do	
-	cut -d: -f1,3 /etc/passwd | awk '$1=$1' | tr -s ' ' ':' | awk -F : '(($2>=1000)&&($1!="nfsnobody")){print $1}' | cat -n | awk '$1=$1' > result.txt #вывести список пользователей, пронумеровав его и убрав повторяющиеся пробелы	
-	cat result.txt
+	tmp_result="$(mktemp)"
+	tmp_findResult="$(mktemp)"
+	tmp_userPasswd="$(mktemp)"
+	cut -d: -f1,3 /etc/passwd | awk '$1=$1' | tr -s ' ' ':' | awk -F : '(($2>=1000)&&($1!="nfsnobody")){print $1}' | cat -n | awk '$1=$1' > $tmp_result #вывести список пользователей, пронумеровав его и убрав повторяющиеся пробелы	
+	cat $tmp_result
 	echo "Введите имя пользователя или порядковый номер"
 	read choose
-	grep -w $choose result.txt | cut -d' ' -f2 > findResult.txt #выбор из списка пользователей имени введенного пользователя
-	findResultLen=$(stat -c%s findResult.txt) #определение количества символов в файле с именем пользователя (если длина файла 0, то вывести "Пользователь не найден")
+	grep -w $choose $tmp_result | cut -d' ' -f2 > $tmp_findResult #выбор из списка пользователей имени введенного пользователя
+	findResultLen=$(stat -c%s $tmp_findResult) #определение количества символов в файле с именем пользователя (если длина файла 0, то вывести "Пользователь не найден")
 	case $findResultLen in
 		0)echo "Пользователь не найден"
 	 	 echo "Скрипт закончил работу"
-          		rm result.txt 
-			rm findResult.txt
+          		rm $tmp_result 
+			rm $tmp_findResult
 			while ((1));do
         			echo "Повторить?(y/n)"
         			read repeat
@@ -22,6 +27,7 @@ while ((1));do
                 			"y")repeats=1
                         			break;;
                 			"n")echo "Выполняется выход..."
+						echo "---------------------------------------"
                         			repeats=0
                         			break;;
                 			*)echo "Неверный ввод";;
@@ -34,30 +40,31 @@ while ((1));do
 		fi
 			;;
 		*)echo "Найден пользователь: " 
-			cat findResult.txt
+			cat $tmp_findResult
 			;;
 	esac
 	while read line #записать имя пользователя в переменную foundUser
 		do foundUser=$line
 		break
-	done < findResult.txt
-	cat /etc/shadow | grep $foundUser | cut -d: -f2 > userPasswd.txt #записать пароль пользователя из /etc/shadow в userPasswd.txt
-	if grep -q '\!' userPasswd.txt
+	done < $tmp_findResult
+	cat /etc/shadow | grep $foundUser | cut -d: -f2 > $tmp_userPasswd #записать пароль пользователя из /etc/shadow в userPasswd
+	if grep -q '\!\!' $tmp_userPasswd
 		then echo "Пользователь заблокирован, разблокировка невозможна, так как при разблокировке появится пользователь без пароля. Измените пароль пользователю и запустите скрипт еще раз."
-		rm result.txt
-		rm findResult.txt
-		rm userPasswd.txt
+		rm $tmp_result 
+		rm $tmp_findResult
+		rm $tmp_userPasswd
+		echo "---------------------------------------"
 		exit 0
-	elif grep -q '\!.' userPasswd.txt #при блокировке пользователя в начало его пароля добавляется восклицательный знак, соответственно необходимо проверить его наличие
+	elif grep -q '\!.' $tmp_userPasswd #при блокировке пользователя в начало его пароля добавляется восклицательный знак, соответственно необходимо проверить его наличие
 		then echo "Пользователь заблокирован"
 		echo "Разблокировать?(y/n)"
 		read decision
 		case $decision in
 			"y") usermod -U $foundUser
 	       			echo "Скрипт завершил работу"
-	       			rm result.txt
-	       			rm findResult.txt
-	       			rm userPasswd.txt
+	       			rm $tmp_result 
+	       			rm $tmp_findResult
+	       			rm $tmp_userPasswd
 	       			while ((1));do
         				echo "Повторить?(y/n)"
         				read repeat
@@ -65,6 +72,7 @@ while ((1));do
                 				"y")repeats=1
                         				break;;
                 				"n")echo "Выполняется выход..."
+							echo "---------------------------------------"
                         				repeats=0
                         				break;;
              		   			*)echo "Неверный ввод";;
@@ -77,9 +85,9 @@ while ((1));do
 			fi
 			;;
 			"n")echo "Скрипт завершил работу"
-	      			rm result.txt
-              			rm findResult.txt
-              			rm userPasswd.txt
+	      			rm $tmp_result 
+              			rm $tmp_findResult
+              			rm $tmp_userPasswd
 	     	 		while ((1));do
         				echo "Повторить?(y/n)"
         				read repeat
@@ -87,6 +95,7 @@ while ((1));do
                 				"y")repeats=1
                         				break;;
                 				"n")echo "Выполняется выход..."
+							echo "---------------------------------------"
                         				repeats=0
                         				break;;
                 				*)echo "Неверный ввод";;
@@ -99,15 +108,15 @@ while ((1));do
 			fi
 			;;
 			*)echo "Неверный ввод"
-	      			rm result.txt
-              			rm findResult.txt
-              			rm userPasswd.txt
+	      			rm $tmp_result 
+              			rm $tmp_findResult
+              			rm $tmp_userPasswd
 				;;	
 		esac
 	else echo "Пользователь разблокирован"
-		rm result.txt
-		rm findResult.txt
-		rm userPasswd.txt
+		rm $tmp_result 
+		rm $tmp_findResult
+		rm $tmp_userPasswd
 	fi
 	while ((1));do
 		echo "Повторить?(y/n)"
@@ -116,6 +125,7 @@ while ((1));do
 			"y")repeats=1
 				break;;
 			"n")echo "Выполняется выход..."
+				echo "---------------------------------------"
 				repeats=0
 				break;;
 			*)echo "Неверный ввод";;
